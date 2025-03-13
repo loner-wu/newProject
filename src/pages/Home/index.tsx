d545db215b65e33e7f37ca21bed44f249a48510d
loner-wu <13870662360@163.com>
2025-03-14 00:52:45 +0800

    feat: 路由管理

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home=()=>{
+    return <span>123</span>
+}
+
+export default Home;
---
+++ src/pages/Manage/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+// import "./index.module.less";
+import './index.css'
+
+const Manage = () => {
+  return <span className="manage">管理页面</span>;
+};
+
+export default Manage;
